8d008a54569ddeb0ca1557c63fbbe948fe75fd67
Gabriel Solomon <hello@gabrielsolomon.ro>
2024-12-04 19:22:45 +0200

    fix: update cacheable action

--- src/Behaviours/Cacheable.php
+++ src/Behaviours/Cacheable.php
@@ -3,40 +3,19 @@
 namespace Bytic\Actions\Behaviours;
 
 use Nip\Cache\Cacheable\CanCache;
-use Nip\Utility\Str;
-use Nip\Utility\Time\DurationValues;
 
 trait Cacheable
 {
     use CanCache;
+
     public function handle()
     {
-        return $this->cacheStore()->remember(
-            $this->cacheName(),
-            $this->dataCacheTtl(),
-            function () {
-                return $this->generateCacheData();
-            }
-        );
+        return $this->getDataFromCache();
     }
 
     public static function forgetCache()
     {
         $self = new self(...func_get_args());
         $self->cacheStore()->delete($self->cacheName());
-    }
-
-    protected function cacheName()
-    {
-        return $this->dataCacheKey();
-    }
-
-    protected function dataCacheKey($key = null)
-    {
-        $base = Str::slug(__CLASS__);
-        if ($key) {
-            $base .= '.' . $key;
-        }
-        return $base;
     }
 }
